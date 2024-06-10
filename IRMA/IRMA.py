d82991f497ec9a0ef398612c55eb25a974e67d92
aknijn <arnold.knijn@iss.it>
2024-06-10 14:05:15 +0200

    args

--- IRMA/IRMA.py
+++ IRMA/IRMA.py
@@ -60,37 +60,37 @@ def __main__():
     if consensus_HA:
         if os.path.isfile(consensus_HA[0]):
             sequenced_region_list.append('HA')
-            shutil.copy(consensus_HA[0], 'consensus_HA')
+            shutil.copy(consensus_HA[0], args.consensus_HA)
             h_subtype_str = get_subtype(consensus_HA[0], 'H')
         consensus_NA = glob.glob('outdir/*NA*.fasta')
         if consensus_NA and os.path.isfile(consensus_NA[0]):
             sequenced_region_list.append('NA')
-            shutil.copy(consensus_NA[0], 'consensus_NA')
+            shutil.copy(consensus_NA[0], args.consensus_NA)
             n_subtype_str = get_subtype(consensus_NA[0], 'N')
         consensus_MP = glob.glob('outdir/*MP*.fasta')
         if consensus_MP and os.path.isfile(consensus_MP[0]):
             sequenced_region_list.append('MP')
-            shutil.copy(consensus_MP[0], 'consensus_MP')
+            shutil.copy(consensus_MP[0], args.consensus_MP)
         consensus_PB1 = glob.glob('outdir/*PB1*.fasta')
         if consensus_PB1 and os.path.isfile(consensus_PB1[0]):
             sequenced_region_list.append('PB1')
-            shutil.copy(consensus_PB1[0], 'consensus_PB1')
+            shutil.copy(consensus_PB1[0], args.consensus_PB1)
         consensus_PB2 = glob.glob('outdir/*PB2*.fasta')
         if consensus_PB2 and os.path.isfile(consensus_PB2[0]):
             sequenced_region_list.append('PB2')
-            shutil.copy(consensus_PB2[0], 'consensus_PB2')
+            shutil.copy(consensus_PB2[0], args.consensus_PB2)
         consensus_PA = glob.glob('outdir/*PA*.fasta')
         if consensus_PA and os.path.isfile(consensus_PA[0]):
             sequenced_region_list.append('PA')
-            shutil.copy(consensus_PA[0], 'consensus_PA')
+            shutil.copy(consensus_PA[0], args.consensus_PA)
         consensus_NP = glob.glob('outdir/*NP*.fasta')
         if consensus_NP and os.path.isfile(consensus_NP[0]):
             sequenced_region_list.append('NP')
-            shutil.copy(consensus_NP[0], 'consensus_NP')
+            shutil.copy(consensus_NP[0], args.consensus_NP)
         consensus_NS = glob.glob('outdir/*NS*.fasta')
         if consensus_NS and os.path.isfile(consensus_NS[0]):
             sequenced_region_list.append('NS')
-            shutil.copy(consensus_NS[0], 'consensus_NS')
+            shutil.copy(consensus_NS[0], args.consensus_NS)
         if os.path.isfile('outdir/A_MP.fasta'):
             influenza_type_str = 'A'
         if os.path.isfile('outdir/B_MP.fasta'):
